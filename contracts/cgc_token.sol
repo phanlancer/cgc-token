ba817c8f32e7e39daaf6e705444dd534e146ed50
Phan Trung Sinh <phanlancerduk@hotmail.com>
2020-01-30 07:42:32 +0700

    change interface

--- contracts/cgc_token.sol
+++ contracts/cgc_token.sol
@@ -50,22 +50,22 @@ contract SafeMath {
 // ----------------------------------------------------------------------------
 // ERC Token Standard #20 Interface
 // ----------------------------------------------------------------------------
-contract ERC20Interface {
-    function totalSupply() public view returns (uint256);
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
     function balanceOf(address tokenOwner)
-        public
+        external
         view
         returns (uint256 balance);
     function allowance(address tokenOwner, address spender)
-        public
+        external
         view
         returns (uint256 remaining);
-    function transfer(address to, uint256 tokens) public returns (bool success);
+    function transfer(address to, uint256 tokens) external returns (bool success);
     function approve(address spender, uint256 tokens)
-        public
+        external
         returns (bool success);
     function transferFrom(address from, address to, uint256 tokens)
-        public
+        external
         returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint256 tokens);
